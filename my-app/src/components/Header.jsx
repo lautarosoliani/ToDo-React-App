a5569ba28a9843cdde6299b928b9345462ece4e7
Lautaro <lautarosoliani@hotmail.com>
2021-08-23 20:18:42 -0300

    app.js, body jsx and css, header jsx and css, index css

--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -2,7 +2,7 @@ import "./Header.css"
 
 const Header = () => {
     return (
-        <div className="container p-3 my-3 text-white">
+        <div className="header container p-3 my-3 text-white">
             <h1>From Header - Taskinator</h1>
         </div>
     )
