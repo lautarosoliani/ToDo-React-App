67f765f9eb4ec8cc272c5cc2c7d0efc65618fbb5
Lautaro <lautarosoliani@hotmail.com>
2021-08-23 16:23:53 -0300

    bootstrap added

--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -2,8 +2,8 @@ import "./Header.css"
 
 const Header = () => {
     return (
-        <div className="container">
-            <h1>From Header</h1>
+        <div className="container p-3 my-3 bg-primary text-white">
+            <h1>From Header - Taskinator</h1>
         </div>
     )
 }
