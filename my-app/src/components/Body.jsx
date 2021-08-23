b3003c0256f7b6780ee7667f105385d18842f290
Lautaro <lautarosoliani@hotmail.com>
2021-08-23 20:36:31 -0300

    body css and jsx, header css

--- my-app/src/components/Body.jsx
+++ my-app/src/components/Body.jsx
@@ -2,9 +2,22 @@ import "./Body.css"
 
 const Body = () => {
     return (
-        <div className="body container p-3 my-3 text-white">
-            <h1>From Body - Taskinator</h1>
-        </div>
+        <>
+            <div className="body container p-3 my-3 text-white">
+                <div className="h1">
+                    <h1>From Body - Taskinator</h1>
+                </div>
+                <div className="select">
+                    <select>
+                        <option>Select Category</option>
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                    </select>
+                </div>
+            </div>
+        </>
     )
 }
 
