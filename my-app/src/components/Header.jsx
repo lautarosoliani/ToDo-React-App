c12b15bbf41323e5018fc8b808f591982cdd569f
Lautaro <lautarosoliani@hotmail.com>
2021-08-23 16:28:50 -0300

    Header refactored

--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -2,7 +2,7 @@ import "./Header.css"
 
 const Header = () => {
     return (
-        <div className="container p-3 my-3 bg-primary text-white">
+        <div className="container p-3 my-3 text-white">
             <h1>From Header - Taskinator</h1>
         </div>
     )
