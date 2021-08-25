c0da79d0f7f60d22141474c359cc3be87eb8f6dc
Lautaro <lautarosoliani@hotmail.com>
2021-08-24 23:42:07 -0300

    Various

--- my-app/src/components/FormTodo.jsx
+++ my-app/src/components/FormTodo.jsx
@@ -1,7 +1,31 @@
-import React from "react"
+import React, { useState } from "react"
 
-const FormTodo = () => {
-    return <>FormTodo</>
+const FormTodo = (props) => {
+    const [description, setDescription] = useState("")
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(description)
+        setDescription("")
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <div>
+                    <input
+                        type="text"
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                    />
+                    <button
+                        className="btn btn-primary"
+                        disabled={description ? "" : "disabled"}
+                    >
+                        Add
+                    </button>
+                </div>
+            </div>
+        </form>
+    )
 }
 
 export default FormTodo
